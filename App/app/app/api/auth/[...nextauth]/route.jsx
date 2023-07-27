94dffe8145118a7f77afb6e769b20f738641664b
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-07-27 23:15:57 +0500

    doing

---
+++ App/app/app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth/next";
+import prisma from "../../../lib/prismadb";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+// import bcrypt from 'bcrypt'
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" },
+                username: { label: "Username", type: "text", placeholder: "John Smith" },
+            },
+            async authorize(credentials) {
+              
+               const user = {name:"saad"};
+               return user;
+            },
+        }),  
+    ],
+    secret: process.env.SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    debug: process.env.NODE_ENV === "development",
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST}
--- App/app/app/page.js
+++ App/app/app/page.js
@@ -1,6 +1,11 @@
-
-export default function Home() {
+import { getServerSession } from "next-auth";
+import {authOptions} from "./api/auth/[...nextauth]/route";
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
+    <>
     <h1>Hello world</h1>
+    <h1>{JSON.stringify(session)}</h1>
+    </>
   )
 }
